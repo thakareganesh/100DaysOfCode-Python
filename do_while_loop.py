077d59093ad81fc5b7369facac811d248b665cff
thakareganesh <thakareganesh803@gmail.com>
2024-03-08 11:42:15 +0530

    Day 7

---
+++ do_while_loop.py
@@ -0,0 +1,15 @@
+# hurdle race practice hurdle 3
+# it won't run 
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_left()
+    move()
+    turn_left()
+    move()
+    turn_left()
