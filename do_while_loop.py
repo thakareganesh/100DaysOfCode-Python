52ee67e110078130a846d654004cded6b85fc7c0
thakareganesh <thakareganesh803@gmail.com>
2024-03-08 13:24:52 +0530

    Day 7

--- do_while_loop.py
+++ do_while_loop.py
@@ -4,8 +4,9 @@ def turn_right():
     turn_left()
     turn_left()
     turn_left()
+
 def jump():
-    move()
+    #move()
     turn_left()
     move()
     turn_left()
@@ -13,3 +14,35 @@ def jump():
     turn_left()
     move()
     turn_left()
+
+while not at_goal():
+    if wall_in_front():
+        jump()
+    else:
+        move()
+
+
+
+# hurdle 4
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    #move()
+    turn_left()
+    while wall_on_right():
+        move()
+    turn_right()
+    move()
+    turn_right()
+    while front_is_clear():
+        move()
+    turn_left()
+    
+while not at_goal():
+    if wall_in_front():
+        jump()
+    else:
+        move()
