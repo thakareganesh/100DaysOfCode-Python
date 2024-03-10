2e6b2ca0fdcb8763109048c8afdbd70389c5a81e
thakareganesh <thakareganesh803@gmail.com>
2024-03-10 19:15:35 +0530

    In progress

--- FreeCodeCamp/recursion_tower_of_hanoi.py
+++ FreeCodeCamp/recursion_tower_of_hanoi.py
@@ -1,7 +1,32 @@
+NUMBER_OF_DISKS = 3
+number_of_moves = 2 ** NUMBER_OF_DISKS - 1
+#print(number_of_moves)
 rods = {
-    'A': range(3, 0, -1),
+    'A': list(range(NUMBER_OF_DISKS, 0, -1)),
     'B': [],
     'C': []
 }
 # print(rods['A'])
-print(type(rods["A"]))
+#deprint(type(rods["A"]))
+def move(n, source, auxiliary, target):
+    # display starting configuration
+    print(rods)
+    for i in range(number_of_moves):
+        remainder = (i + 1) % 3
+        if remainder == 1:
+            print(f"Move {i + 1} allowed between {source} and {target}")
+            forward = False
+            if not rods[target]:
+                forward = True
+            elif rods[source] and rods[source][-1] < rods[target][-1]:
+                forward = True
+            if forward:
+                print(f'Moving disk {rods[source][-1]} from {source} to {target}')
+                rods[target].append(rods[source].pop())
+        elif remainder == 2:
+            print(f"Move {i + 1} allowed between {source} and {auxiliary}")
+        elif remainder == 0:
+            print(f"Move {i + 1} allowed between {auxiliary} and {target}")
+
+# initiate call from source A to target C with auxiliary B
+move(NUMBER_OF_DISKS, 'A', 'B', 'C')
