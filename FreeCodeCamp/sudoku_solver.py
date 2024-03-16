1a67b0560bb1d915e2e2ddbb7a0ab7f96042d984
thakareganesh <thakareganesh803@gmail.com>
2024-03-16 11:40:25 +0530

    added try and except clause

--- FreeCodeCamp/sudoku_solver.py
+++ FreeCodeCamp/sudoku_solver.py
@@ -24,5 +24,10 @@ class Board:
             else:
                 board_string += lower_lines
         return board_string
-
-
+    def find_empty_cell(self):
+        for row, contents in enumerate(self.board):
+            try:
+                col = contents.index(0)
+                return row, col
+            except ValueError:
+                pass
