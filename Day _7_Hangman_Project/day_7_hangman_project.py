7aa81c16327fb388acff56f5f94d6fbba2e0538c
thakareganesh <thakareganesh803@gmail.com>
2024-03-22 09:17:44 +0530

    Added Replit Module

--- Day _7_Hangman_Project/day_7_hangman_project.py
+++ Day _7_Hangman_Project/day_7_hangman_project.py
@@ -1,11 +1,6 @@
-#Step 5
-# from replit import clear
+from replit import clear
 import random
 
-#TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
-# import hangman_words
-# chosen_word = random.choice(hangman_words.word_list)
-# word_length = len(chosen_word)
 from hangman_words import word_list
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
@@ -13,11 +8,8 @@ word_length = len(chosen_word)
 end_of_game = False
 lives = 6
 
-#TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
 from hangman_art import logo
 print(logo)
-#Testing code 
-print(f'Pssst, the solution is {chosen_word}.')
 
 #Create blanks
 display = []
@@ -26,8 +18,8 @@ for _ in range(word_length):
 
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
+    clear()
 
-    #TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
     if guess in display:
         print(f"You've already guessed {guess}")
 
@@ -40,7 +32,6 @@ while not end_of_game:
 
     #Check if user is wrong.
     if guess not in chosen_word:
-        #TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
         print(f"You guessed {guess}, that's not in the word. You lose a life.")
         lives -= 1
         if lives == 0:
@@ -55,6 +46,5 @@ while not end_of_game:
         end_of_game = True
         print("You win.")
 
-    #TODO-2: - Import the stages from hangman_art.py and make this error go away.
     from hangman_art import stages
     print(stages[lives])
