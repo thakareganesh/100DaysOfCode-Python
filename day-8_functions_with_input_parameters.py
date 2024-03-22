505d2c0b4c9a287f18868fdd039951bcae165a03
thakareganesh <thakareganesh803@gmail.com>
2024-03-22 15:25:45 +0530

    simple function example

--- day-8_functions_with_input_parameters.py
+++ day-8_functions_with_input_parameters.py
@@ -1,4 +1,7 @@
-print("test")
-print("test2")
-print("=" * 5)
-git 
+# simple function
+def greet():
+    print("Hello Ganesh!")
+    print("How do you do Sanket Patil?")
+    print("In'nt it the weather nice today?")
+
+greet()
