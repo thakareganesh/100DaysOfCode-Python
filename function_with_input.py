676eb3fb6cb113b5abe55a4214d63b3e2e884ebb
thakareganesh <thakareganesh803@gmail.com>
2024-03-23 05:06:49 +0530

    added function_with_input.py

---
+++ function_with_input.py
@@ -0,0 +1,8 @@
+# functions that are allow for input
+
+def greet_with_name(name):
+    print(f"Hello  {name}")
+    print(f"How do you do {name}")
+   
+your_name = input("what is your name? ")
+greet_with_name(your_name)
