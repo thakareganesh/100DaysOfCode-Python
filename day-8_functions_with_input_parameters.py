acf5ff384774480fee7fded5a30d63313c170ef6
thakareganesh <thakareganesh803@gmail.com>
2024-03-23 04:45:11 +0530

    added simple_function_example.py

--- day-8_functions_with_input_parameters.py
+++ day-8_functions_with_input_parameters.py
@@ -1,7 +1,14 @@
 # simple function
-def greet():
-    print("Hello Ganesh!")
-    print("How do you do Sanket Patil?")
-    print("In'nt it the weather nice today?")
+# def greet():
+#     print("Hello Ganesh!")
+#     print("How do you do Sanket Patil?")
+#     print("In'nt it the weather nice today?")
 
-greet()
+# greet()
+
+def greet_with_name(name):
+  print(f"Hello {name}")
+  print(f"How do you do {name}?")
+
+uname = input("what is your name?\n")
+greet_with_name(uname)
