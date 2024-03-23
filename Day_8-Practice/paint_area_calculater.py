3352197f350c9c6c63af409640aa90cfc4374413
thakareganesh <thakareganesh803@gmail.com>
2024-03-23 10:15:41 +0530

    challange completed

--- Day_8-Practice/paint_area_calculater.py
+++ Day_8-Practice/paint_area_calculater.py
@@ -1,4 +1,3 @@
-# Write your code below this line 👇
 import math
 def paint_calc(height,width, cover):
   num_of_cans = (height * width) / cover
@@ -7,11 +6,6 @@ def paint_calc(height,width, cover):
   print(f"You'll need {round_up_cans} cans of paint.")
 
 
-
-# Write your code above this line 👆
-# Define a function called paint_calc() so the code below works.   
-
-# 🚨 Don't change the code below 👇
 test_h = int(input("enter height of wall: ")) # Height of wall (m)
 test_w = int(input("enter width of wall: ")) # Width of wall (m)
 coverage = 5
