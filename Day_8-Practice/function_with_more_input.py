d88a701d9595c3a4af0a5d601990bc6eb35debcb
thakareganesh <thakareganesh803@gmail.com>
2024-03-23 09:39:08 +0530

    understood function with more input

--- Day_8-Practice/function_with_more_input.py
+++ Day_8-Practice/function_with_more_input.py
@@ -1,1 +1,18 @@
  # fucntions with more than one inputs
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}?")
+
+# postional arguments
+greet_with("Ganesh", "Nanded")
+
+print(f"=" *28)
+
+# keyword argumentts
+greet_with(name="Ganesh", location="Nanded")
+
+print(f"=" *28)
+
+# or
+
+greet_with(location="Nanded", name="Ganesh")
